0e423344c9793644b1e3b32b5c2142fe362b42db
Đặng Vũ Quang Minh <dvqminh98@gmail.com>
2022-12-03 17:53:25 +0700

    Final Commit

---
+++ match3-clone/Assets/Script/Board.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public int offSet;
+    public GameObject tilePrefabs;
+    private BackgroundTile[,] allTiles;
+    public GameObject[] dots;
+    public GameObject[,] allDots;
+    public int remainToDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width,height];
+        allDots = new GameObject[width, height];
+        Setup();
+        Debug.Log(remainToDestroy);
+    }
+
+    private void Update()
+    {
+        if(remainToDestroy == 0)
+        {
+            UIManager.instance.OnEndGame();
+        }
+    }
+    private void Setup()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j + offSet);
+                GameObject backgroundTile = Instantiate(tilePrefabs, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "( " + i + ", " + j + " )";
+                int dotToUse = Random.Range(0, dots.Length);
+
+                int maxIteration = 0;
+                while (MatchesAt(i, j, dots[dotToUse]) && maxIteration < 100)
+                {
+                    dotToUse = Random.Range(0, dots.Length);
+                }
+                maxIteration = 0;
+
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.GetComponent<Dot>().row = j;
+                dot.GetComponent<Dot>().column = i;
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + ", " + j + " )";
+                allDots[i, j] = dot;
+            }
+        }
+    }
+
+    private bool MatchesAt(int column, int row, GameObject piece)
+    {
+        if(column >1 && row > 1)
+        {
+            if(allDots[column-1, row].tag == piece.tag
+                && allDots[column-2,row].tag == piece.tag)
+            {
+                return true;
+            }
+            if (allDots[column, row - 1].tag == piece.tag
+                && allDots[column, row - 2].tag == piece.tag)
+            {
+                return true;
+            }
+        }else if(column <= 1 || row <= 1)
+        {
+            if(row > 1)
+            {
+                if(allDots[column, row -1].tag == piece.tag
+                    && allDots[column,row-2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allDots[column -1, row].tag == piece.tag
+                    && allDots[column -2,row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void DestroyMatchesAt(int column, int row)
+    {
+        if(allDots[column,row].GetComponent<Dot>().isMatched)
+        {
+            Destroy(allDots[column, row]);
+            allDots[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i, j] != null)
+                {
+                    DestroyMatchesAt(i,j);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+        remainToDestroy--;
+        Debug.Log(remainToDestroy);
+    }
+
+    private IEnumerator DecreaseRowCo()
+    {
+        int nullCount = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i, j] == null)
+                {
+                    nullCount++;
+                }else if(nullCount > 0)
+                {
+                    allDots[i, j].GetComponent<Dot>().row -= nullCount;
+                    allDots[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoardCo());
+    }
+
+    private void RefillBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(allDots[i,j] == null)
+                {
+                    Vector2 tempPosition = new Vector2(i, j+offSet);
+                    int dotToUse = Random.Range(0, dots.Length);
+                    GameObject piece = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                    allDots[i, j] = piece;
+                    piece.GetComponent<Dot>().row = j;
+                    piece.GetComponent<Dot>().column = i;
+                }
+            }
+        }
+    }
+
+    private bool MatchesOnBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(allDots[i,j]!= null)
+                {
+                    if (allDots[i, j].GetComponent<Dot>().isMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FillBoardCo()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(.5f);
+
+        while (MatchesOnBoard())
+        {
+            yield return new WaitForSeconds(.2f);
+            DestroyMatches();
+        }
+    }
+}
---
+++ match3-clone/Assets/Script/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject pauseButton;
+    public GameObject endGameUI;
+    public static UIManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void OnPauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void OnResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+    }
+    public void OnRestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void OnEndGame()
+    {
+        endGameUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
